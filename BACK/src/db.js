b8aceceb5ac2a02494cc090d23d7a84a0d159cd7
NachoAbait <102827148+NachoAbait@users.noreply.github.com>
2023-05-14 18:37:03 -0300

    actualizaciones para el deploy

--- BACK/src/db.js
+++ BACK/src/db.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 
+/*
 const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`,
   {
@@ -11,6 +12,12 @@ const sequelize = new Sequelize(
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   }
 );
+*/
+const sequelize = new Sequelize(DB_DEPLOY, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+});
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
@@ -43,8 +50,8 @@ const { Recipe, Diet_Types } = sequelize.models;
 // Product.hasMany(Reviews);
 
 // RECIPE <--------> DIET_TYPE
-Recipe.belongsToMany(Diet_Types, {through: "Recipe_Diet_type" });
-Diet_Types.belongsToMany(Recipe, {through: "Recipe_Diet_type" });
+Recipe.belongsToMany(Diet_Types, { through: "Recipe_Diet_type" });
+Diet_Types.belongsToMany(Recipe, { through: "Recipe_Diet_type" });
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
